5d5a795a8e67a6f5c24a871156897d1268b7e928
tripabhi <abhijittripathy.projects@gmail.com>
2021-10-17 03:13:36 -0400

    Boilerplate setup

---
+++ app/communicator/__init__.py
@@ -0,0 +1,5 @@
+from .communicator import Communicator
+from .s3_comm import S3Communicator
+from .s3_primitive import *
+from .s3_primitive_nn import *
+
---
+++ app/storage/__init__.py
@@ -0,0 +1,2 @@
+from .base import BaseStorage
+from .s3.s3_type import S3Storage
